cbc769ce2e3bc9a1daeb54c48f7b3eb4de50350d
Lenny Reis <142733648+ReisLcb@users.noreply.github.com>
2025-04-07 15:45:48 +0000

    Update home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, NgFor],
 })
 export class HomePage {
   constructor() {}
+  num = Math.floor(Math.random() * 3)
+  portas = [1, 2, 3]
+
+  abrirPorta(numero:number){
+    switch(numero){
+      case 0:
+        alert("Cabra")
+        break;
+
+      case 1:
+        alert("Carro")
+        break;
+
+      case 2:
+        alert("Cabra")
+          break;
+    }
+  }
 }
